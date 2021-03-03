c31f81115c42c725036ea931b8edaa3c4d9ed7a0
jbenatem <jbenatem@gmail.com>
2021-03-03 01:05:21 -0500

    webchat button added

---
+++ directline-chat-reactjs/src/components/WebChat.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ChatButton from './ChatButton';
+
+class WebChat extends Component{
+    render(){
+        return(
+            <ChatButton/>
+        );
+    }
+}
+export default WebChat;

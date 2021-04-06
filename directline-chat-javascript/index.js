0d704911484711db0d8e26c555010457fdb8f6ca
jbenatem <jbenatem@gmail.com>
2021-04-06 16:47:33 -0500

    cleaning code

--- directline-chat-javascript/index.js
+++ directline-chat-javascript/index.js
@@ -9,7 +9,7 @@ function createGuid() {
 }  
 
 async function generateDirectLineToken() {
-    const secret = 'uzEqtToqsAY.jFKlI0ltLDzLjq0Kj_lD2YPTDempUQdxriJ7g3GWB-g';
+    const secret = 'INSERT-DIRECT-LINE-SECRET-HERE';
     var guid = createGuid();
     const response = await fetch('https://directline.botframework.com/v3/directline/tokens/generate', {
         method: 'POST',
